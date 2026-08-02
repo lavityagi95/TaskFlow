a43ba92cbb21b159fe26ca9153a8b0c45e71a051
lavityagi95 <lavityagiyt@gmail.com>
2026-08-03 00:38:30 +0530

    Setup bcrypt for password hashing

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,64 +1,70 @@
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
-const registerPage = (req, res) => {
+
+const registerPage = async (req, res) => {
     res.render("company/register");
 };
 
 const registerCompany = (req, res) => {
 
-    const {
-        company_name,
-        owner_name,
-        email,
-        password
-    } = req.body;
+    // Get form data
+    const { company_name, owner_name, email, password } = req.body;
 
-    // Validation
-
+    // Validate form
     if (!company_name || !owner_name || !email || !password) {
-
         return res.send("All fields are required.");
-
     }
 
-    const sql = `
-        INSERT INTO companies
-        (company_name, owner_name, email, password)
-        VALUES (?, ?, ?, ?)
+    // Check if email already exists
+    const checkEmailSql = `
+        SELECT id
+        FROM companies
+        WHERE email = ?
     `;
 
-    const checkEmailSql = `
-    SELECT * FROM companies
-    WHERE email = ?
-`;
-
-db.query(checkEmailSql, [email], (err, result) => {
-
-    if (err) {
-        console.error(err);
-        return res.send("Database Error");
-    }
-
-    console.log(result);
-
-});
-
-    db.query(sql, [company_name, owner_name, email, password], (err, result) => {
+    db.query(checkEmailSql, [email], (err, result) => {
 
         if (err) {
-
             console.error(err);
-
             return res.send("Database Error");
-
         }
 
-        res.send("Company Registered Successfully");
+        // Email already registered
+        if (result.length > 0) {
+            return res.send("Email already registered.");
+        }
+
+        // Insert new company
+        const insertSql = `
+            INSERT INTO companies
+            (company_name, owner_name, email, password)
+            VALUES (?, ?, ?, ?)
+        `;
+
+        db.query(
+            insertSql,
+            [company_name, owner_name, email, password],
+            (err, result) => {
+
+                if (err) {
+                    console.error(err);
+                    return res.send("Database Error");
+                }
+
+                console.log("Company Registered Successfully");
+                console.log("Company ID:", result.insertId);
+
+                res.send("Company Registered Successfully");
+
+            }
+        );
 
     });
 
 };
+
 module.exports = {
     registerPage,
-    registerCompany,
+    registerCompany
 };
